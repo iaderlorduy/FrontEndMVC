e53284376e4d03fb6e1072b8738179b39ff13335
iaderlorduy <iaderlor@gmail.com>
2015-09-22 11:24:03 -0500

    Consumir Servicio desde Azure MVC

---
+++ FrontEndAPI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI.Models;
+
+namespace FrontEndAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            //
+
+            var httpClient = new HttpClient();
+            //var json = await httpClient.GetStringAsync("http://ilorduywebapi.azurewebsites.net/api/Artistas");
+            var json = await httpClient.GetStringAsync("http://jloracudris.azurewebsites.net/api/Artistas");
+            var ArtistaList = JsonConvert.DeserializeObject<List<Artista>>(json);
+
+
+            return View(ArtistaList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FrontEndAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrontEndAPI.Startup))]
+namespace FrontEndAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
